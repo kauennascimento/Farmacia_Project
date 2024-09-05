5c01ce95be604723acc43920115d058b776e34ca
Kaue <githubkaue@gmail.com>
2024-09-05 16:11:07 -0300

    Cardápio

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily:{
+      'sans': ['Roboto Condensed', 'sans-serif']
+    },
+    extend: {
+      backgroundImage:{
+        "home": "url('/assets/bg.png')"
+      }
+    },
+  },
+  plugins: [],
+}
+
